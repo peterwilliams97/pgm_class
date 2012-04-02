f4c55c5096cb01e05e017f49fde488b2187cdfa2
Peter Williams <peter.williams.97@gmail.com>
2012-04-02 21:18:18 +1000

    Done with q9

--- quiz1/q9.py
+++ quiz1/q9.py
@@ -1,8 +1,10 @@
 from __future__ import division
 
+# Priors
 Ppresident = [0.99, 0.01]
 Paccident = [0.9, 0.1]
 
+# Conditionals
 # P[p,a] = P(traffic=1|president=p,accident=a)
 Ptraffic = {}
 Ptraffic[0,0] = 0.1
@@ -11,25 +13,27 @@ Ptraffic[1,0] = 0.6
 Ptraffic[1,1] = 0.9
 
 def margin(president, accident):
+    """Return P(traffic=1) marginalized over sets of values of president 
+        and accident
+        president and accident can both be [0], [1] or [0,1]
+        """ 
     return sum([Ptraffic[(p,a)]*Ppresident[p]*Paccident[a] 
          for p in president
          for a in accident])
 
 # P(accident=1,traffic=1)
-Pat = Paccident[1] * sum([Ptraffic[p,1]*Ppresident[p] for p in (0,1)])
 Pat = margin(president=[0,1], accident=[1])
 
 # P(traffic=1)
-Pt = sum([Ptraffic[(p,a)]*Ppresident[p]*Paccident[a] for a in (0,1) for p in (0,1)])
 Pt = margin(president=[0,1], accident=[0,1])
 
 # P(accident=1 | traffic=1) = P(accident=1,traffic=1)/P(traffic=1) 
 Pa_t = Pat/Pt
 
+print '-' * 80
 print 'Pat  = %f' % Pat
 print 'Pt   = %f' % Pt
 print 'Pa_t = %f' % Pa_t
-print '-' * 80
 
 #Patp = P(Accident=1, Traffic=1, President=1)
 Patp = margin(president=[1], accident=[1])
@@ -41,7 +45,7 @@ Ptp = margin(president=[1], accident=[0,1])
 # = P(Accident=1, Traffic=1, President=1) / P(Traffic=1, President=1)
 Pa_tp = Patp/Ptp
 
+print '-' * 80
 print 'Patp  = %f' % Patp
 print 'Ptp   = %f' % Ptp
 print 'Pa_tp = %f' % Pa_tp
-print '-' * 80
